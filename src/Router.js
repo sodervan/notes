d4b20cc95ae00d63e86c4f39b343fb10b741258b
sodervan0 <egbowonemmanuel0000@gmail.com>
2024-05-02 16:50:35 +0100

    commitiooon

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ import Signup from "./Auth/Signup.vue";
 import Login from "./Auth/Login.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     // { path: "/", redirect: "/allnotes" },
     { path: "/", redirect: "/signup" },
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/notes4you/" : "/",
   plugins: [vue()],
 });
